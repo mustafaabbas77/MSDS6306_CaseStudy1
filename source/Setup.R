7bfea13979a93656f718f97b24a41ea146d58364
Thomas Wang <thomaswang@utexas.edu>
2017-03-19 20:36:58 -0500

    Add files via upload

---
+++ source/Setup.R
@@ -0,0 +1,8 @@
+if (!require("dplyr")) {
+  install.packages("dplyr", repos="http://cran.rstudio.com/") 
+}
+library(dplyr)
+if (!require("ggplot2")) {
+  install.packages("ggplot2", repos="http://cran.rstudio.com/") 
+}
+library(ggplot2)
